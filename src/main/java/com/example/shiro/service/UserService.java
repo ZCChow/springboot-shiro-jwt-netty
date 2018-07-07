e290a3bc49139b7f13bd32f26c2d4ec963faf7bd
ZCChow <258422801@qq.com>
2018-07-07 20:50:12 +0800

    测试覆盖去除

--- src/main/java/com/example/shiro/service/UserService.java
+++ src/main/java/com/example/shiro/service/UserService.java
@@ -6,16 +6,4 @@ import java.util.List;
 
 public interface UserService {
     User findByUserName(String userName);
-    void InerstUser(User user);
-
-    void InerstUser2(User user);
-    void InerstUser4(User user);
-    void InerstUser5(User user);
-    void InerstUser6(User user);
-    void InerstUser3(User user);
-    void InerstUser7(User user);
-    void InerstUser8(User user);
-    void InerstUser9(User user);
-    void InerstUser10(User user);
-    void InerstUser11(User user);
 }
